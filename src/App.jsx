182beb941f94658cb65c46dc83a608f7fdb7d1d7
Kundan Tripathi <kundantripathi1512@gmail.com>
2024-09-06 16:40:49 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useReducer } from 'react';
 import Header from './components/Header';
 import Loader from './components/Loader';
 import Error from './components/Error';
@@ -10,68 +9,6 @@ import Timer from './components/Timer';
 import NextButton from './components/NextButton';
 import Main from './Main.jsx';
 import {useQuiz } from './context/QuizContext.jsx';
-
-// const initialState = {
-//   questions: [],
-//   //loading, error, ready, active, finished
-//   status: "loading",
-//   index: 0,
-//   answer: null,
-//   points: 0,
-//   highscore: 0,
-//   secondsRemaining: null
-// }
-
-// const SECS_PER_QUE = 30;
-
-// function reducer(state, action){
-//   switch(action.type){
-//     case "dataReceived": {
-//       return {...state, questions: action.payLoad, status: "ready"}
-//     }
-//     case "dataFailed": {
-//       return {...state, status: "error"}
-//     }
-//     case "start": {
-//       return {...state, status: "active", secondsRemaining: state.questions.length * SECS_PER_QUE}
-//     }
-//     case "newAnswer": {
-//       const question = state.questions.at(state.index);
-//       return {
-//         ...state,
-//         answer: action.payLoad,
-//         points: action.payLoad === question.correctOption ? state.points + question.points : state.points
-//       }
-//     }
-//     case "nextQuestion": {
-//       return {
-//         ...state,
-//         index: state.index + 1,
-//         answer: null
-//       }
-//     }
-//     case "finished": {
-//       return {
-//         ...state,
-//         status: "finished",
-//         highscore: state.points > state.highscore ? state.points : state.highscore
-//         }
-//     }
-//     case "restart": {
-//       return {...state, status: "ready", index: 0, points: 0, secondsRemaining: 5, answer: null}
-//     }
-//     case "tick": {
-//       return {...state, 
-//         secondsRemaining: state.secondsRemaining - 1, 
-//         status: state.secondsRemaining === 0 ? "finished" : state.status, 
-//         highscore: state.secondsRemaining === 0 ? state.points > state.highscore ? state.points : state.highscore : state.highscore
-//       }
-//     }
-//     default: {
-//       throw new Error("Action Unknown");
-//     }
-//   }
-// }
 
 
 const App = () => {
