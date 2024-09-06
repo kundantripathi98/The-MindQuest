9caac59713310d63d2539df1152b49f201ea32d2
Kundan Tripathi <kundantripathi1512@gmail.com>
2024-09-06 16:39:10 +0530

    updated

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,12 +1,14 @@
-import { useEffect, useState } from "react";
+import { useEffect} from "react";
+import { useQuiz } from "../context/QuizContext";
 
-const Timer = ({dispatcher, secondsRemaining}) => {
+const Timer = () => {
+    const {dispatcher, secondsRemaining} = useQuiz();
+
     const formatTime = (seconds) => {
         const minutes = Math.floor(seconds / 60);
         const remainingSeconds = seconds % 60;
         return `0${minutes} : ${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
       };
-
 
     useEffect(()=>{
         let id = setInterval(()=>{
