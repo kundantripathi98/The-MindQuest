2f68e8756fcde181eba8dcdd2bbdc34f1f447054
Kundan Tripathi <kundantripathi1512@gmail.com>
2024-09-06 16:38:03 +0530

    updated

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,11 +1,16 @@
-const Result = ({points, maxPossiblePoints, dispatcher, highscore}) => {
+import { useQuiz } from "../context/QuizContext";
+
+const Result = () => {
+    const {points, maxPossiblePoints, dispatcher, highscore} = useQuiz();
     const percentage = Math.round((points / maxPossiblePoints) * 100);
     let emoji;
+
     if(percentage === 100) emoji = "🥇";
     if(percentage >=80 && percentage < 100) emoji = "🥳";
     if(percentage >=50 && percentage < 80) emoji = "😁";
     if(percentage >= 0 && percentage < 50) emoji = "🤔";
     if(percentage == 0) emoji = "🤦";
+
   return (
     <>
       <p className="result"><span>{emoji}</span>You scored {points} out of {maxPossiblePoints} ({percentage}%)</p>
